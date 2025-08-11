77104876d9859d36e01af54c9cdfa6da0b36395f
thom1606 <info@thomvandenbroek.com>
2025-08-11 10:12:24 +0200

    Initial Commit

---
+++ ShareBox/ShareBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareBoxApp.swift
+//  ShareBox
+//
+//  Created by Thom van den Broek on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShareBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
